4e286bbe4c3000d4e676d1d03597a146094a705f
ibaialberdi <ibaialberdi@gmail.com>
2025-07-31 20:07:49 +0200

    clean and comment errors
    - Removed unused SimpleLayout import from App component.
    - Added ESLint directive to sidebar exports to suppress warnings for component exports.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import SignInPage from "./pages/auth/signin"
 import SignUpPage from "./pages/auth/signup"
 import DashboardPage from "./pages/dashboard/dashboard"
 import DashboardLayout from "./layouts/dashboard-layout"
-import SimpleLayout from "./layouts/simple-layout"
 
 function App() {
   return (
